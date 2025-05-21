693ecb6fee5e71465454d8f5435f055378b4cb1e
Manjula.V <manjulavenkatappa57@gmail.com>
2025-05-21 17:34:43 +0530

    backend and express is added to github

---
+++ backend/01-WebServer/server_node.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Send response from server");
+  } else if (request.url === "/about") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Tell about server");
+  } else {
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "text/plain");
+    response.end("404 File not found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is listening on http://${hostname}:${port}`);
+});
